1d5a2705c17223ba01c8531286cc7b1e621d792b
expressiveone31-oss <expressive.one31@gmail.com>
2025-10-28 14:25:58 +0300

    Delete bot directory

--- bot/keyboards_common.py
+++
@@ -1,7 +0,0 @@
-
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def cancel_kb() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(inline_keyboard=[[
-        InlineKeyboardButton(text="Отмена", callback_data="cancel")
-    ]])
