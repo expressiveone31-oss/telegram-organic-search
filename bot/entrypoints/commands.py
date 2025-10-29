d0ac5ff5fecde5daa52fa94044e6d17abf326cf3
expressiveone31-oss <expressive.one31@gmail.com>
2025-10-29 08:58:01 +0300

    Delete bot directory

--- bot/entrypoints/commands.py
+++
@@ -1,6 +0,0 @@
-
-from aiogram import Dispatcher
-from bot.handlers.telemetr_search import router as telemetr_router
-
-def setup_routers(dp: Dispatcher) -> None:
-    dp.include_router(telemetr_router)
